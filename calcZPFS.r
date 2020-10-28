d865b94bf088fff6317e0cfa6dfc934dc553fbc1
Joske Ubels <joske.ubels@gmail.com>
2020-10-28 20:54:14 +0100

    Update calcZPFS.r

--- calcZPFS.r
+++ calcZPFS.r
@@ -89,7 +89,7 @@ calcZPFS = function(surv.z, gep, nbrs = 10, sets, toi = "YES"){
     names(zpfs) <- treatment
     
     for(i in 1:length(treatment)){
-      true.nbr[,i] <- rownames(distanceA)[sort(distanceA[,i], decreasing = FALSE, index = TRUE)$ix[2:(nbrs+1)]]
+      true.nbr[,i] <- rownames(distanceA)[sort(distanceA[,i], decreasing = FALSE, index = TRUE)$ix[1:nbrs]]
       true.dpfs[i] <- mean(surv.diff[true.nbr[,i],i], na.rm = TRUE)
     } 
     
@@ -117,7 +117,7 @@ calcZPFS = function(surv.z, gep, nbrs = 10, sets, toi = "YES"){
     names(zpfs) <- treatment.2
     
     for(i in 1:length(treatment.2)){
-      true.nbr[,i] <- rownames(distanceB)[sort(distanceB[,i], decreasing = FALSE, index = TRUE)$ix[2:(nbrs+1)]]
+      true.nbr[,i] <- rownames(distanceB)[sort(distanceB[,i], decreasing = FALSE, index = TRUE)$ix[1:nbrs]]
       true.dpfs[i] <- mean(surv.diff.2[true.nbr[,i],i], na.rm = TRUE)
     } 
     
